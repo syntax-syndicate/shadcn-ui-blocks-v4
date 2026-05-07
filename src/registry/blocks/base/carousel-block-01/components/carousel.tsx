e70ed815986276aa7193136837983798cd9d86a2
akash3444 <akashmoradiya3444@gmail.com>
2026-05-07 09:36:33 +0530

    feat: enhance carousel blocks

--- src/registry/blocks/base/carousel-block-01/components/carousel.tsx
+++ src/registry/blocks/base/carousel-block-01/components/carousel.tsx
@@ -18,49 +18,46 @@ const images = [
 
 export default function CarouselDemo() {
   return (
-    <div className="flex min-h-screen items-center justify-center px-6 md:px-14">
-      <div>
-        <div className="flex items-end justify-between">
-          <div>
-            <h2 className="font-medium text-3xl">dddepth</h2>
-            <p className="mt-2 text-pretty text-lg text-muted-foreground leading-snug">
-              A Curated Collection of AI-generated Abstract 3D Shapes
-            </p>
+    <div className="mx-auto max-w-5xl px-14 py-20">
+      <div className="flex items-end justify-between">
+        <div>
+          <h2 className="font-medium text-3xl tracking-tight">dddepth</h2>
+          <p className="mt-2 text-pretty text-lg text-muted-foreground leading-snug">
+            A Curated Collection of AI-generated Abstract 3D Shapes
+          </p>
+        </div>
+        <Button className="max-sm:hidden" size="sm" variant="outline">
+          <Link href="https://www.fffuel.co/dddepth/" target="_blank">
+            View all
+          </Link>
+        </Button>
+      </div>
+      <Carousel className="mt-6 w-full" opts={{ loop: true, align: "start" }}>
+        <CarouselContent>
+          {images.map((image, index) => (
+            <CarouselItem
+              className="basis-1/2 md:basis-1/3 lg:basis-1/4"
+              key={index}
+            >
+              <div className="p-1">
+                <div className="aspect-square">
+                  <img className="rounded-lg object-cover" src={image} />
+                </div>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <div className="mt-4 flex items-center justify-between sm:justify-end">
+          <div className="flex items-center justify-end gap-1.5">
+            <CarouselPrevious className="-left-10 max-md:static max-md:translate-y-0" />
+            <CarouselNext className="-right-10 max-md:static max-md:translate-y-0" />
           </div>
-          <Button className="max-sm:hidden" size="sm" variant="outline">
+
+          <Button className="sm:hidden" size="sm" variant="outline">
             <Link href="https://www.fffuel.co/dddepth/">View all</Link>
           </Button>
         </div>
-        <Carousel
-          className="mt-6 w-full max-w-screen-lg"
-          opts={{ loop: true, align: "start" }}
-        >
-          <CarouselContent>
-            {images.map((image, index) => (
-              <CarouselItem
-                className="basis-1/2 md:basis-1/3 lg:basis-1/4"
-                key={index}
-              >
-                <div className="p-1">
-                  <div className="aspect-square">
-                    <img className="rounded-lg object-cover" src={image} />
-                  </div>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <div className="mt-4 flex items-center justify-between sm:justify-end">
-            <div className="flex items-center justify-end gap-1.5">
-              <CarouselPrevious className="-left-10 max-md:static max-md:translate-y-0" />
-              <CarouselNext className="-right-10 max-md:static max-md:translate-y-0" />
-            </div>
-
-            <Button className="sm:hidden" size="sm" variant="outline">
-              <Link href="https://www.fffuel.co/dddepth/">View all</Link>
-            </Button>
-          </div>
-        </Carousel>
-      </div>
+      </Carousel>
     </div>
   );
 }
--- src/registry/blocks/radix/carousel-block-02/components/carousel.tsx
+++ src/registry/blocks/radix/carousel-block-02/components/carousel.tsx
@@ -16,19 +16,19 @@ const images = [
 
 export default function CarouselDemo() {
   return (
-    <div className="flex min-h-screen items-center justify-center px-6">
+    <div className="px-6 py-20">
       <Carousel
-        className="mt-6 w-full max-w-screen-lg"
+        className="mx-auto w-full max-w-5xl"
         opts={{ loop: true, align: "start" }}
       >
         <div className="mb-3 flex items-center justify-end gap-1 md:hidden">
           <CarouselPrevious className="static translate-y-0" />
           <CarouselNext className="static translate-y-0" />
         </div>
-        <div className="rounded-xl bg-background p-6 shadow-xl/5 ring ring-border/40">
+        <div className="rounded-xl bg-card p-6 shadow-xl/3 ring ring-border/80">
           <div className="mb-5 flex items-end justify-between">
             <div>
-              <h2 className="font-medium text-3xl">dddepth</h2>
+              <h2 className="font-medium text-3xl tracking-tight">dddepth</h2>
               <p className="mt-2 text-lg text-muted-foreground leading-snug">
                 A Curated Collection of AI-generated Abstract 3D Shapes
               </p>
